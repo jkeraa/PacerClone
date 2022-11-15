dd00e7d1a88fd039c64f9651c79b899cd73c0500
Jacques André Kerambrun <jakerambrun@libero.it>
2022-11-15 19:16:56 +0100

    Initial Commit

---
+++ Pacer/PacerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PacerApp.swift
+//  Pacer
+//
+//  Created by Jacques André Kerambrun on 15/11/22.
+//
+
+import SwiftUI
+
+@main
+struct PacerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
